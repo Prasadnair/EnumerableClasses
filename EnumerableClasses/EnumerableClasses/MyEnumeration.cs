9b00066112038d0788f2defff45a2735d67e5ab7
Prasadnair <prasadnair44@gmail.com>
2024-03-22 23:29:16 -0400

    enumerable classes

---
+++ EnumerableClasses/EnumerableClasses/MyEnumeration.cs
@@ -0,0 +1,24 @@
+﻿namespace EnumerableClasses
+{
+    public class MyEnumeration
+    {
+        public static readonly MyEnumeration Value1 = new MyEnumeration(1, "Value 1");
+        public static readonly MyEnumeration Value2 = new MyEnumeration(2, "Value 2");
+        public static readonly MyEnumeration Value3 = new MyEnumeration(3, "Value 3");
+
+        public int Id { get; }
+        public string Name { get; }
+
+        private MyEnumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Optionally, you can override ToString() for better string representation
+        public override string ToString()
+        {
+            return $"{Id}: {Name}";
+        }
+    }
+}
---
+++ EnumerableClasses/EnumerableClasses/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumerableClasses;
+
+Console.WriteLine("Enumerable Class Tutorial");
+MyEnumeration value = MyEnumeration.Value1;
+Console.WriteLine(value); // Output: 1: Value 1
+
+// You can also access Id and Name properties
+Console.WriteLine(value.Id); // Output: 1
+Console.WriteLine(value.Name); // Output: Value 1
